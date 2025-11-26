8efc4397eb51145df08a01e2c931e8e163269977
fbrrzz <165051361+fbrrzz@users.noreply.github.com>
2025-11-26 13:01:43 +0000

    Change file path in ibex_worker_fifo.cpp
    Updated the file path for the System initialization.

--- named_pipes/ibex_worker_fifo.cpp
+++ named_pipes/ibex_worker_fifo.cpp
@@ -26,7 +26,7 @@ int main() {
     }
 
     
-    System sys("/home/labeit/Ibex/ibex-lib/benchs/optim/easy/ex3_1_3.bch");
+    System sys("/home/labeit/Ibex/ibex-lib/benchs/optim/easy/ex3_1_3.bch"); //Cambiar la ruta
 
     string line;
 
